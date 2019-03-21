41eb86b82b68854275847c4d8d59e7bdd01ae21a
creative-zombie-4458 <jtbartlow@gmail.com>
2019-03-21 15:58:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   first_array = string.split(" ")
   new_array = []
  first_array.each do | string |
-  new_array << string
+  new_array << string.reverse
   end
   new_array.join(" ")
 
